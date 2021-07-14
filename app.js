fff8582f9fcdce509330319b7e958c24571a4e7b
sarthak264 <sarthaksharm372@gmail.com>
2021-07-15 00:10:27 +0530

    Colors are showing in input field

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 let boxes = document.querySelectorAll('.box')
 let btn = document.getElementById('btn')
 
-btn.addEventListener('click',function(){
-    boxes.forEach(function(box) {
-        box.style.backgroundColor = randomColor();
+btn.addEventListener('click',main)
+
+main();
+
+function main() {
+    boxes.forEach(function(box,num) {
+        color = randomColor();
+        box.style.backgroundColor = color;
+        inputBox = boxes[num].querySelector('input')
+        inputBox.value = color;
     })
-})
-boxes.forEach(function(box) {
-    box.style.backgroundColor = randomColor();
-})
+}
 
 function randomColor() {
     const digits = "1234567890ABCDEF"
     let color = "#"
-
     for(let i = 0; i <6; i++) {
         color += digits[Math.floor(Math.random() * 16)]
     }
-    console.log(color);
     return color;
 }
